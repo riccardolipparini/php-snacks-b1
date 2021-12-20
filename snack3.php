7171c41f4dd2a74c4b6182c2aa6516a483a38208
Riccardo <riccardolipparini0@gmail.com>
2021-12-20 17:28:52 +0100

    snsck 3 finito

---
+++ snack3.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+
+$posts = [
+
+    '10/01/2019' => [
+        [
+            'title' => 'Post 1',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 1'
+        ],
+        [
+            'title' => 'Post 2',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 2'
+        ],
+    ],
+    '10/02/2019' => [
+        [
+            'title' => 'Post 3',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 3'
+        ]
+    ],
+    '15/05/2019' => [
+        [
+            'title' => 'Post 4',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 4'
+        ],
+        [
+            'title' => 'Post 5',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 5'
+        ],
+        [
+            'title' => 'Post 6',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 6'
+        ]
+    ],
+];
+$keys = array_keys($posts);
+
+    for ($i=0; $i < count($keys) ; $i++) { 
+        for ($x=0; $x < count($posts[$keys[$i]]); $x++) { 
+            echo "<div>" . $keys[$i] . "<br>" . $posts[$keys[$i]][$x]['title'] . "<br>" . $posts[$keys[$i]][$x]['author'] . "<br>" . $posts[$keys[$i]][$x]['text'] ."</div>";
+        }
+    }
+?>
+</div>
+</body>
+</html>
